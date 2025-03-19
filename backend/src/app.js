ecdb676b6cc8c9aad889661c32041124b3fa1d9e
Swarnabha <nandiswarnabha@gmail.com>
2025-03-20 01:26:24 +0530

    cors fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,6 +23,11 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
+app.use(cors({
+    origin: "https://xmeet-z2bw.onrender.com", // Your frontend URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true // Allow cookies and authentication headers
+}));
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
