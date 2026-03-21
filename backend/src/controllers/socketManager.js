c944e0a27537e2bb244c55545b0d726e3bc6d9df
Swarnabha Nandi <nandiswarnabha@gmail.com>
2026-03-21 20:02:10 +0530

    Remove unwanted comments and whitespaces
    Refactor socketManager to streamline connection and message handling, enhancing readability and maintainability.

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,6 +1,4 @@
 import { Server } from "socket.io"
-
-
 let connections = {}
 let messages = {}
 let timeOnline = {}
@@ -15,24 +13,15 @@ export const connectToSocket = (server) => {
         }
     });
 
-
     io.on("connection", (socket) => {
-
         console.log("SOMETHING CONNECTED")
-
         socket.on("join-call", (path) => {
 
             if (connections[path] === undefined) {
                 connections[path] = []
             }
             connections[path].push(socket.id)
-
             timeOnline[socket.id] = new Date();
-
-            // connections[path].forEach(elem => {
-            //     io.to(elem)
-            // })
-
             for (let a = 0; a < connections[path].length; a++) {
                 io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
             }
@@ -54,21 +43,16 @@ export const connectToSocket = (server) => {
 
             const [matchingRoom, found] = Object.entries(connections)
                 .reduce(([room, isFound], [roomKey, roomValue]) => {
-
-
                     if (!isFound && roomValue.includes(socket.id)) {
                         return [roomKey, true];
                     }
-
                     return [room, isFound];
-
                 }, ['', false]);
 
             if (found === true) {
                 if (messages[matchingRoom] === undefined) {
                     messages[matchingRoom] = []
                 }
-
                 messages[matchingRoom].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id })
                 console.log("message", matchingRoom, ":", sender, data)
 
@@ -78,42 +62,25 @@ export const connectToSocket = (server) => {
             }
 
         })
-
         socket.on("disconnect", () => {
-
             var diffTime = Math.abs(timeOnline[socket.id] - new Date())
-
             var key
-
             for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
-
                 for (let a = 0; a < v.length; ++a) {
                     if (v[a] === socket.id) {
                         key = k
-
                         for (let a = 0; a < connections[key].length; ++a) {
                             io.to(connections[key][a]).emit('user-left', socket.id)
                         }
-
                         var index = connections[key].indexOf(socket.id)
-
                         connections[key].splice(index, 1)
-
-
                         if (connections[key].length === 0) {
                             delete connections[key]
                         }
                     }
                 }
-
             }
-
-
         })
-
-
     })
-
-
     return io;
 }
